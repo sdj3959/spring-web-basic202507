f25ba93fa67007cdc91d3cbc1e1cf5cce7b31b7d
sdj3959 <dj9099@naver.com>
2025-07-14 12:00:39 +0900

    컨트롤러 페이지 라우팅

--- src/main/java/com/spring/basic/chap2_4/controller/BookController2_4.java
+++ src/main/java/com/spring/basic/chap2_4/controller/BookController2_4.java
@@ -31,17 +31,23 @@ public class BookController2_4 {
         - 기본값은 id 오름차, title은 가나다순, price는 내림차
      */
     @GetMapping
-    public List<Book> list(@RequestParam(defaultValue = "id") String sort) {
+    public Map<String, Object> list(@RequestParam(defaultValue = "id") String sort) {
 //        List<Book> bookList = new ArrayList<>();
 //        for (Long key : bookStore.keySet()) {
 //            bookList.add(bookStore.get(key));
 //        }
 
-        return new ArrayList<>(bookStore.values())
+        List<Book> bookList = new ArrayList<>(bookStore.values())
                 .stream()
                 .sorted(getComparator(sort))
-                .collect(Collectors.toList())
-                ;
+                .collect(Collectors.toList());
+
+        int count = bookStore.size();
+
+        return Map.of(
+                "count", count,
+                "bookList", bookList
+        );
 
     }
 
@@ -96,7 +102,6 @@ public class BookController2_4 {
             return id + "번 도서는 존재하지 않습니다. 삭제 실패!";
         }
         return "도서 삭제 완료! - " + id;
-
     }
 
     @PutMapping("/{id}")
@@ -108,19 +113,18 @@ public class BookController2_4 {
     ) {
         Book foundBook = bookStore.get(id);
 
-        if  (foundBook == null) {
-            return id+ "번 도서는 존재하지 않습니다.";
+        if (foundBook == null) {
+            return id + "번 도서는 존재하지 않습니다.";
         }
 
         foundBook.updateBookInfo(title, author, price);
 
-        foundBook.setTitle(title);
-        foundBook.setAuthor(author);
-        foundBook.setPrice(price);
         return "도서 수정 완료: id - " + id;
     }
 
     // 책이 몇권 저장됐는지 알려주기
-
-
+    @GetMapping("/count")
+    public String count() {
+        return "현재 저장된 도서의 개수: " + bookStore.size() + "권";
+    }
 }
