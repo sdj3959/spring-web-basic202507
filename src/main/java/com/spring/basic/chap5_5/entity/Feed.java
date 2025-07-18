51d0772ee37a08f1499226752c0226532021113a
sdj3959 <dj9099@naver.com>
2025-07-18 11:59:19 +0900

    피드 관리 기능 추가: Feed 엔터티, API 컨트롤러, 레포지토리 클래스 구현

---
+++ src/main/java/com/spring/basic/chap5_5/entity/Feed.java
@@ -0,0 +1,21 @@
+package com.spring.basic.chap5_5.entity;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+// 게시글 피드 한 개의 내용을 저장
+public class Feed {
+    private Long feedId; // 피드 식별번호
+    private String content; // 피드 내용
+    private String writer; // 작성자명
+    private LocalDateTime createAt; // 작성시간
+    private int viewCount; // 조회수
+}
